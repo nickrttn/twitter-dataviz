98de342846bb52c640ea07d161818a0b5f974d23
Nick Rutten <hello@nickrttn.me>
2017-04-30 19:36:46 +0200

    connect to the Twitter Streaming API and stream to the console

---
+++ routes/dataviz.js
@@ -0,0 +1,34 @@
+const debug = require('debug')('dataviz');
+const Twitter = require('twitter');
+
+module.exports = (req, res) => {
+	res.render('pages/dataviz.ejs', {
+		errors: req.session.errors,
+		user: req.session.screenName
+	});
+
+	if (req.session.error) {
+		req.session.error = null;
+	}
+
+	stream(req.session);
+};
+
+function stream(session) {
+	const client = new Twitter({
+		consumer_key: process.env.TWITTER_APIKEY, // eslint-disable-line camelcase
+		consumer_secret: process.env.TWITTER_APISECRET, // eslint-disable-line camelcase
+		access_token_key: session.oauthToken, // eslint-disable-line camelcase
+		access_token_secret: session.oauthTokenSecret // eslint-disable-line camelcase
+	});
+
+	const stream = client.stream('statuses/sample');
+
+	stream.on('data', evt => {
+		debug(evt && evt.text);
+	});
+
+	stream.on('error', err => {
+		debug(err);
+	});
+}
